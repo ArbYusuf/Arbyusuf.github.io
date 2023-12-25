201704455373de997d76e991830869dae3586b27
ArbYusuf <154565526+ArbYusuf@users.noreply.github.com>
2023-12-25 02:22:42 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-importScripts("./uv.bundle.js");
-importScripts("./uv.config.js");
-importScripts("./uv.sw.js");
-
-const sw = new UVServiceWorker();
-let userKey = new URL(location).searchParams.get('userkey');
-
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
