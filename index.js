2a30600cb9d0555c95e652817522050ad994b042
ArbYusuf <154565526+ArbYusuf@users.noreply.github.com>
2023-12-25 03:43:28 -0500

    STATIC PLEASE BE DONE

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import http from "node:http";
+import createBareServer from "@tomphttp/bare-server-node";
+import path from "node:path";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer("/bare/");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "static")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "index.html"));
+});
+app.get("/science", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "apps.html"));
+});
+app.get("/math", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "games.html"));
+});
+app.get("/english", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "proxy.html"));
+});
+app.get("/settings", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "settings.html"));
+});
+
+
+server.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on("listening", () => {
+  console.log(`Snorlax's Cave listening on port 8080 ${process.env.PORT}`);
+});
+
+server.listen({
+  port: process.env.PORT,
+});
